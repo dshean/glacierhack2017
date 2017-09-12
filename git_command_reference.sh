94df5681b7790eb7f35ede050db89d8ac614f3fe
David Shean <dshean@gmail.com>
2017-09-12 16:15:44 -0700

    Added git_command_reference

---
+++ git_command_reference.sh
@@ -0,0 +1,13 @@
+# Some notes on git commands
+
+# Use github to create a fork of the glacierhack2017 repo
+# https://github.com/dshean/glacierhack2017.git
+
+# Now, click the clone button on your github fork
+# In a terminal, cd to the directory where you keep github repos and code, run the following, substituting the url you just copied
+# git clone https://github.com/USERNAME/glacierhack2017.git
+
+# Now, let's add the remote repo
+# git remote add upstream https://github.com/dshean/glacierhack2017.git
+
+# git pull upstream master
